a64a84cced5feb3f2519d5a91bdfaeebe16571a4
wlrma0108 <dlwlrma0108@gmail.com>
2022-03-20 01:21:24 +0900

    commit

---
+++ Basic_PipeLine_SVM#2.py
@@ -0,0 +1,67 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from scipy import stats
+from sklearn.datasets import make_blobs
+from ipywidgets import interact
+from sklearn.datasets import make_circles
+from mpl_toolkits import mplot3d
+
+plt.style.use('seaborn')
+
+def plot_svc_decision_function(clf, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    x = np.linspace(plt.xlim()[0], plt.xlim()[1], 30)
+    y = np.linspace(plt.ylim()[0], plt.ylim()[1], 30)
+    Y, X = np.meshgrid(y, x)
+    P = np.zeros_like(X)
+    for i, xi in enumerate(x):
+        for j, yj in enumerate(y):
+            P[i, j] = clf.decision_function([[xi, yj]])
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+
+def plot_svm(N=10):
+    X, y = make_blobs(n_samples=200, centers=2,
+                      random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    clf = SVC(kernel='linear')
+    clf.fit(X, y)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='spring')
+    plt.xlim(-1, 4)
+    plt.ylim(-1, 6)
+    plot_svc_decision_function(clf, plt.gca())
+    plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
+                s=200, facecolors='none')
+    
+interact(plot_svm, N=[10, 200], kernel='linear');
+X, y = make_circles(100, factor=.1, noise=.1)
+
+clf = SVC(kernel='linear').fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='spring')
+plot_svc_decision_function(clf);
+r = np.exp(-(X[:, 0] ** 2 + X[:, 1] ** 2))
+
+def plot_3D(elev=30, azim=30):
+    ax = plt.subplot(projection='3d')
+    ax.scatter3D(X[:, 0], X[:, 1], r, c=y, s=50, cmap='spring')
+    ax.view_init(elev=elev, azim=azim)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('r')
+
+interact(plot_3D, elev=(-90, 90), azip=(-180, 180));
+clf = SVC(kernel='rbf')
+clf.fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='spring')
+plot_svc_decision_function(clf)
+plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
+            s=200, facecolors='none');
+
+
